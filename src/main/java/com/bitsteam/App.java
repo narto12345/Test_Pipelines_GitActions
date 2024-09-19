8a8abfeb7386986f78d425f38c708bb65df3ef88
Nicolas <nicosan12@hotmail.com>
2024-09-19 11:14:15 -0500

    pipeline test number 8

--- src/main/java/com/bitsteam/App.java
+++ src/main/java/com/bitsteam/App.java
@@ -1,11 +1,8 @@
 package com.bitsteam;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world! :D, this is a test example");
-
+        System.out.println("2+2=5");
     }
 }
