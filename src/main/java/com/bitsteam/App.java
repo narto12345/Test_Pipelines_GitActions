c5e0a11d241d4d2d04a2b31ed109bc3912368bb6
Nicolas Santiago Sosa Jimenez <nicosan12@hotmail.com>
2024-09-27 16:49:02 -0500

    Update App.java

--- src/main/java/com/bitsteam/App.java
+++ src/main/java/com/bitsteam/App.java
@@ -3,6 +3,6 @@ package com.bitsteam;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world! :D, this is a test example");
-        System.out.println("2+2=5");
+        System.out.println("2+2=5.2");
     }
 }
